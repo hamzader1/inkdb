ed4558de718fb9bce7bff71a80c39a9e0c5198a4
Hamza <official.hamzaderaoui@gmail.com>
2026-07-24 20:58:16 +0100

    Add SqliteDatabse struct with file opening and header parsing

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+#![allow(warnings)]
+mod bytes;
+mod errors;
+mod format;
+mod macros;
+use errors::SqliteDatabaseError;
+use format::header::SqliteDatabaseHeader;
+use std::fs::{File, OpenOptions};
+use std::path::Path;
+
+pub struct SqliteDatabse {
+    file: File,
+    header: SqliteDatabaseHeader,
+}
+impl SqliteDatabse {
+    pub fn new<P: AsRef<Path>>(f_name: P) -> Result<(), SqliteDatabaseError> {
+        let mut file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .open(f_name)
+            .map_err(|err| SqliteDatabaseError::DatabaseOpenFailure(err))?;
+        SqliteDatabaseHeader::parse(&mut file)?;
+        Ok(())
+    }
+}
