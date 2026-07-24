bc0ea7ca7c1c85161543d57dc25d0449bb724565
Hamza <official.hamzaderaoui@gmail.com>
2026-07-24 20:53:40 +0100

    Add binary reader utility functions for integers and arrays

---
+++ src/bytes.rs
@@ -0,0 +1,30 @@
+use crate::{to_int, SqliteDatabaseError};
+use std::io::Read;
+
+pub fn read_u8<R: Read>(r: &mut R) -> Result<u8, SqliteDatabaseError> {
+    let mut buf = [0; 1];
+    r.read_exact(&mut buf)
+        .map_err(|err| SqliteDatabaseError::DatabaseOpenFailure(err));
+    Ok(to_int!(u8, buf))
+}
+
+pub fn read_u16_be<R: Read>(r: &mut R) -> Result<u16, SqliteDatabaseError> {
+    let mut buf = [0; 2];
+    r.read_exact(&mut buf)
+        .map_err(|err| SqliteDatabaseError::DatabaseOpenFailure(err));
+    Ok(to_int!(u16, buf))
+}
+
+pub fn read_u32_be<R: Read>(r: &mut R) -> Result<u32, SqliteDatabaseError> {
+    let mut buf = [0; 4];
+    r.read_exact(&mut buf)
+        .map_err(|err| SqliteDatabaseError::DatabaseOpenFailure(err));
+    Ok(to_int!(u32, buf))
+}
+
+pub fn read_array<const N: usize, R: Read>(r: &mut R) -> Result<[u8; N], SqliteDatabaseError> {
+    let mut buf = [0; N];
+    r.read_exact(&mut buf)
+        .map_err(|err| SqliteDatabaseError::DatabaseOpenFailure(err));
+    Ok(buf)
+}
