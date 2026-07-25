e87939ac256d66a5dcf40563ba6bb561a6315601
Hamza <official.hamzaderaoui@gmail.com>
2026-07-25 20:45:34 +0100

    Implement the page method on SqliteDatabse to allow reading and parsing specific database pages by their page number.

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,10 @@ mod macros;
 mod util;
 use errors::SqliteDatabaseError;
 use format::header::SqliteDatabaseHeader;
+use format::page::BTreePage;
+pub use format::varint::{decode_varint, encode_varint};
 use std::fs::{File, OpenOptions};
+use std::io::{Cursor, Read, Seek, SeekFrom};
 use std::path::Path;
 
 pub struct SqliteDatabse {
@@ -25,5 +28,29 @@ impl SqliteDatabse {
     }
     pub fn header(&self) -> &'_ SqliteDatabaseHeader {
         &self.header
+    }
+
+    pub fn page(&mut self, page_no: u32) -> Result<BTreePage, SqliteDatabaseError> {
+        let mut bytes_to_read: u32 = {
+            if self.header.database_page_size == 1 {
+                (u16::MAX as u32) + 1u32
+            } else {
+                self.header.database_page_size as _
+            }
+        };
+        let offset;
+        if page_no == 1 {
+            offset = 100;
+            bytes_to_read -= 100;
+        } else {
+            offset = ((bytes_to_read * page_no) - bytes_to_read) as u64;
+        }
+        self.file.seek(SeekFrom::Start(offset));
+
+        let mut buff = vec![0u8; bytes_to_read as usize];
+        self.file.read_exact(&mut buff);
+        let mut cur = Cursor::new(&mut buff);
+
+        BTreePage::parse::<Cursor<&mut Vec<u8>>>(&mut cur)
     }
 }
