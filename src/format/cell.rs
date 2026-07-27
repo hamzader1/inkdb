afacd69f811349d9ae7eb3d3c4eeb6fa6a22c5b3
Hamza <official.hamzaderaoui@gmail.com>
2026-07-27 20:56:50 +0100

    Add TableLeafCell struct to represent leaf cells in the BTree structure

--- src/format/cell.rs
+++ src/format/cell.rs
@@ -10,6 +10,7 @@ use super::page::{CellPointer, PageNumber};
 #[derive(Debug)]
 pub enum BTreeCell {
     TableInterior(TableInteriorCell),
+    TableLeaf(TableLeafCell),
     None,
 }
 
@@ -19,6 +20,13 @@ pub struct TableInteriorCell {
     rowid_boundary: u64,
 }
 
+#[derive(Debug)]
+pub struct TableLeafCell {
+    payload_len: u64,
+    row_id: u64,
+    local_payload_range: Range<usize>,
+    first_overflow_page: Option<PageNumber>,
+}
 impl TableInteriorCell {
     pub fn parse<R: Read + Seek>(
         r: &mut R,
