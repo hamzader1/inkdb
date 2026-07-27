bc187fb1bae47304ac482dca82ac41e0bed498a0
Hamza <official.hamzaderaoui@gmail.com>
2026-07-27 20:54:28 +0100

    Implement the compute_local_payload_size utility function to calculate the local payload size based on the usable page size and total payload length

--- src/util.rs
+++ src/util.rs
@@ -5,3 +5,20 @@ pub fn assert_one(condition: bool, err: SqliteDatabaseError) -> Result<(), Sqlit
     }
     Ok(())
 }
+
+pub const fn compute_local_payload_size(usable_size: usize, payload_len: usize) -> usize {
+    let u = usable_size;
+    let p = payload_len;
+    let x = u - 35;
+    if p <= x {
+        return p;
+    } else {
+        let m = ((u - 12) * 32 / 255) - 23;
+        let k = m + (p - m) % (u - 4);
+        if k <= x {
+            return k;
+        } else {
+            return m;
+        }
+    }
+}
