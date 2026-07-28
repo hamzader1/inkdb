51ce7b048a5de1eb1aeb7de13a262b33b617cf1b
Hamza <official.hamzaderaoui@gmail.com>
2026-07-28 17:39:23 +0100

    Implement the OverflowPage struct along with its new constructor to parse overflow page data

--- src/format/overflow.rs
+++ src/format/overflow.rs
@@ -1,3 +1,25 @@
+use std::io::{Cursor, Read};
+
+use crate::bytes::read_u32_be;
+use crate::{to_int, DbError};
+
+use super::page::PageNumber;
+
+pub struct OverflowPage {
+    pub next: PageNumber,
+    pub data: Vec<u8>,
+}
+impl OverflowPage {
+    pub fn new(bytes: Vec<u8>, usable_size: usize) -> Result<Self, DbError> {
+        let mut cursor = Cursor::new(&bytes);
+        // let next_page_buffer = &bytes[0..4];
+        let next = read_u32_be(&mut cursor)?;
+        let mut data = vec![0u8; usable_size - 4];
+        cursor.read_exact(&mut data)?;
+        Ok(Self { next, data })
+    }
+}
+
 /*
    ** X is U-35 for table btree leaf pages or ((U-12)*64/255)-23 for index pages.
    ** M is always ((U-12)*32/255)-23.
