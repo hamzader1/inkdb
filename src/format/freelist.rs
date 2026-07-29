60463a3995cf4dfc698a0b164524f0ca89f7385a
Hamza <official.hamzaderaoui@gmail.com>
2026-07-29 14:19:51 +0100

    Validate the initial freelist trunk page against database constraints before iterating through the freelist to prevent potential reading of invalid memory pages.

--- src/format/freelist.rs
+++ src/format/freelist.rs
@@ -12,26 +12,26 @@ pub struct FreeList {
 
 impl SqliteDatabse {
     pub fn freelist(&mut self) -> Result<Option<FreeList>, DbError> {
+        let mut current_page = self.header.first_freelist_trunk_page;
         if self.header.first_freelist_trunk_page == 0 {
-            return Ok(None); // No FreeList
+            return Ok(None);
+        } else if let Err(e) = self.validate_page(current_page, None::<fn() -> bool>) {
+            return Err(e);
         }
         let mut trunk_pages = Vec::<u32>::new();
         let mut leaf_pages = Vec::<u32>::new();
         let mut buf = vec![0u8; self.header.database_page_size as _];
-        let mut current_page_no = self.header.first_freelist_trunk_page;
 
-        while current_page_no > 0 {
-            self.read_raw_page_into(current_page_no, &mut buf)?;
+        while current_page > 0 {
+            self.read_raw_page_into(current_page, &mut buf)?;
             let mut cursor = Cursor::new(&mut buf);
-            trunk_pages.push(current_page_no);
+            trunk_pages.push(current_page);
             let next_freelist_trunk = read_u32_be(&mut cursor)?;
             let leaf_pages_cnt = read_u32_be(&mut cursor)?;
-            if leaf_pages_cnt > 0 {
-                for _ in 0..leaf_pages_cnt {
-                    leaf_pages.push(read_u32_be(&mut cursor)?);
-                }
+            for _ in 0..leaf_pages_cnt {
+                leaf_pages.push(read_u32_be(&mut cursor)?);
             }
-            current_page_no = next_freelist_trunk;
+            current_page = next_freelist_trunk;
         }
         assert!(
             trunk_pages.len() + leaf_pages.len() == self.header.total_number_of_freelist_pages as _
