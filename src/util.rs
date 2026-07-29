d37cde21c90c6c422b0fca5d6759e41d64431434
Hamza <official.hamzaderaoui@gmail.com>
2026-07-29 17:15:49 +0100

    Rename assert_one and add sqlite_assert_with_corrupt_err

--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,14 @@
 use crate::SqliteDatabaseError;
-pub fn assert_one(condition: bool, err: SqliteDatabaseError) -> Result<(), SqliteDatabaseError> {
+pub fn sqlite_assert_one(condition: bool, err: SqliteDatabaseError) -> Result<(), SqliteDatabaseError> {
     if !condition {
         return Err(err);
+    }
+    Ok(())
+}
+
+pub fn sqlite_assert_with_corrupt_err(condition: bool, err: &str) -> Result<(), SqliteDatabaseError> {
+    if !condition {
+        return Err(SqliteDatabaseError::Corrupt(err.into()));
     }
     Ok(())
 }
