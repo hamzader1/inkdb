e2a11371051dcf7df8b47043de951bb4c6656506
Hamza <official.hamzaderaoui@gmail.com>
2026-08-02 16:57:26 +0100

    Add SqliteMetadata struct to pager module

---
+++ src/pager/metadata.rs
@@ -0,0 +1,19 @@
+use crate::sqlite_assert_all;
+
+#[rustfmt::skip]
+struct SqliteMetadata {
+    page_size:           usize,
+    usable_size:         usize,
+    max_allocated_pages: usize,
+}
+
+impl SqliteMetadata {
+    fn new(page_size: usize, usable_size: usize, max_allocated_pages: usize) -> Self {
+        sqlite_assert_all!(page_size >= usable_size, max_allocated_pages > 0);
+        Self {
+            page_size,
+            usable_size,
+            max_allocated_pages,
+        }
+    }
+}
