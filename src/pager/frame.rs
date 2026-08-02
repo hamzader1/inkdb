66757197293346a1e72633ab1bdb96b74a8e4a63
Hamza <official.hamzaderaoui@gmail.com>
2026-08-02 17:32:21 +0100

    Export Frame struct, add FRAME_SIZE, and implement Default

--- src/pager/frame.rs
+++ src/pager/frame.rs
@@ -1,11 +1,27 @@
 use crate::format::page::PageNo;
+use crate::size_of;
 
 const FREE: u8 = 1 << 0;
 const CLEAN: u8 = 1 << 1;
 const DIRTY: u8 = 1 << 2;
 const REFERENCED: u8 = 1 << 3;
-struct Frame {
-    page_no: PageNo,
+
+pub const FRAME_SIZE: usize = size_of!(Frame);
+pub type FrameId = usize;
+pub type FrameIndex = usize;
+#[derive(Clone, Debug)]
+pub struct Frame {
+    page_no: Option<PageNo>,
     flags: u8,
     pin_count: u8,
+}
+
+impl Default for Frame {
+    fn default() -> Self {
+        Self {
+            page_no: None,
+            flags: FREE,
+            pin_count: 0,
+        }
+    }
 }
