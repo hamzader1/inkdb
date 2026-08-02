5050c086f543baad13289db090d18ba1b85e8427
Hamza <official.hamzaderaoui@gmail.com>
2026-08-02 17:31:48 +0100

    Implement BufferPool struct and initialization logic
    Implement the BufferPool struct with cache capacity, page mapping,
    and memory buffers to support page management and caching operations.

--- src/pager/buffer_pool.rs
+++ src/pager/buffer_pool.rs
@@ -1,1 +1,36 @@
-pub struct BufferPool;
+use super::frame::FRAME_SIZE;
+use super::frame::{Frame, FrameId, FrameIndex};
+use crate::format::page::PageNo;
+use crate::pager::page;
+use std::collections::HashMap;
+
+const CACHE_CAPACITY: usize = 4096;
+#[rustfmt::skip]
+pub struct BufferPool {
+    page_map    : HashMap<PageNo, FrameId>,
+    page_buffer : Box<[u8]>,
+    frame_buffer: Box<[Frame]>,
+    free_frames : Vec<FrameId>,
+    clock_hand  : FrameIndex
+}
+
+impl BufferPool {
+    fn new(page_size: usize) -> Self {
+        // todo: check overflow of CacheCap * Psize
+        let page_buffer = Self::make_box_buffer::<u8>(CACHE_CAPACITY * page_size);
+        // todo: check overflow of CacheCap * Fsize
+        let frame_buffer = Self::make_box_buffer::<Frame>(CACHE_CAPACITY * FRAME_SIZE);
+        let free_frames = Vec::<FrameId>::with_capacity(CACHE_CAPACITY);
+
+        Self {
+            page_map: HashMap::new(),
+            page_buffer,
+            frame_buffer,
+            free_frames,
+            clock_hand: 0,
+        }
+    }
+    pub fn make_box_buffer<T: Clone + Default>(size: usize) -> Box<[T]> {
+        vec![T::default(); size].into_boxed_slice()
+    }
+}
