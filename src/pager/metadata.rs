a340c3fde9b58b1028f114b90a3dd7b6395230ef
Hamza <official.hamzaderaoui@gmail.com>
2026-08-03 00:39:58 +0100

    Make SqliteMetadata struct and its fields and constructor public

--- src/pager/metadata.rs
+++ src/pager/metadata.rs
@@ -1,14 +1,14 @@
 use crate::sqlite_assert_all;
 
 #[rustfmt::skip]
-struct SqliteMetadata {
-    page_size:           usize,
-    usable_size:         usize,
-    max_allocated_pages: usize,
+pub struct SqliteMetadata {
+    pub page_size           : usize,
+    pub usable_size         : usize,
+    pub max_allocated_pages : usize,
 }
 
 impl SqliteMetadata {
-    fn new(page_size: usize, usable_size: usize, max_allocated_pages: usize) -> Self {
+    pub fn new(page_size: usize, usable_size: usize, max_allocated_pages: usize) -> Self {
         sqlite_assert_all!(page_size >= usable_size, max_allocated_pages > 0);
         Self {
             page_size,
