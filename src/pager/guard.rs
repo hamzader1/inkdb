4dee109e8ca9d164d6b6574e8f72bc549cc37c4c
Hamza <official.hamzaderaoui@gmail.com>
2026-08-02 20:15:14 +0100

    Add get_bytes and get_bytes_mut to PageBytes, and expose them via bytes_as_re and bytes_as_mut on PageGuard.

--- src/pager/guard.rs
+++ src/pager/guard.rs
@@ -21,6 +21,21 @@ impl<'b> PageBytes<'b> {
     pub fn new_refmut(bytes: &'b mut Vec<u8>) -> Self {
         Self::RefMutBytes(bytes)
     }
+    fn get_bytes(&self) -> &'_ Vec<u8> {
+        match self {
+            Self::RefBytes(bytes) => bytes,
+
+            // allowed to get &Vec if you have &mut Vec
+            Self::RefMutBytes(bytes) => bytes,
+        }
+    }
+    fn get_bytes_mut(&mut self) -> &mut Vec<u8> {
+        if let Self::RefMutBytes(bytes) = self {
+            bytes
+        } else {
+            panic!("You can't get mut bytes from immutable page shared reference")
+        }
+    }
 }
 impl<'b> PageGuard<'b> {
     pub fn new_ref(bufferpool: NonNull<BufferPool>, bytes: &'b Vec<u8>, frame_id: FrameId) -> Self {
@@ -43,6 +58,13 @@ impl<'b> PageGuard<'b> {
             bytes: PageBytes::new_refmut(bytes),
             _marker: PhantomData,
         }
+    }
+    pub fn bytes_as_ref(&self) -> &Vec<u8> {
+        self.page_bytes.get_bytes()
+    }
+
+    pub fn bytes_as_mut(&mut self) -> &mut Vec<u8> {
+        self.page_bytes.get_bytes_mut()
     }
 }
 
