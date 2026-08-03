9e5125e1a4791f58fff85a75cc504cd3a290ed27
Hamza <official.hamzaderaoui@gmail.com>
2026-08-03 19:18:13 +0100

    Integrate Pager into SqliteDatabase and initialize in with_source

--- src/lib.rs
+++ src/lib.rs
@@ -20,15 +20,16 @@ use self::vfs::cursor::FileCursor;
 use self::vfs::disk::{DiskFile, DiskVfs};
 use self::vfs::file::SqliteFile;
 use self::vfs::Vfs;
+use crate::pager::page::Pager;
 
 pub struct SqliteDatabase<S: SqliteFile> {
-    source: S,
+    pager: Pager<S>,
     header: SqliteDatabaseHeader,
 }
 
 impl SqliteDatabase<DiskFile> {
     pub fn new<P: AsRef<Path>>(db_path: P) -> Result<Self, SqliteError> {
-        let mut sqlite_default_vfs = DiskVfs;
+        let sqlite_default_vfs = DiskVfs;
         Self::with_source(sqlite_default_vfs, db_path)
     }
 }
@@ -41,10 +42,16 @@ impl<'f, S: SqliteFile> SqliteDatabase<S> {
     {
         let source = vfs.open(path, SqliteOptions::default())?;
         let header = SqliteDatabaseHeader::parse(&source)?;
-        Ok(Self { source, header })
+        let pager = Pager::new(
+            source,
+            header.database_page_size as _,
+            (header.database_page_size - header.reserved_space as u32) as _,
+            header.database_size_in_pages as _,
+        );
+        Ok(Self { pager, header })
     }
     fn source(&'f self) -> &'f S {
-        &self.source
+        &self.pager.source
     }
     fn cursor(&self) -> FileCursor<'_, S> {
         FileCursor::new(self.source())
@@ -62,7 +69,7 @@ impl<'f, S: SqliteFile> SqliteDatabase<S> {
         let offset = page_size * (page_no - 1);
 
         let mut buff = vec![0u8; page_size as usize];
-        self.source.read_exact_at(offset as u64, &mut buff)?;
+        self.pager.source.read_exact_at(offset as u64, &mut buff)?;
 
         BTreePage::parse(
             buff,
@@ -91,16 +98,12 @@ impl<'f, S: SqliteFile> SqliteDatabase<S> {
         let buff = buff.as_mut();
         // self.file.seek(SeekFrom::Start(offset as u64))?;
 
-        self.source.read_exact_at(offset as _, buff)?;
+        self.pager.source.read_exact_at(offset as _, buff)?;
 
         Ok(())
     }
 
-    fn validate_page<F>(
-        &mut self,
-        page_no: PageNo,
-        exception: Option<F>,
-    ) -> Result<(), SqliteError>
+    fn validate_page<F>(&mut self, page_no: PageNo, exception: Option<F>) -> Result<(), SqliteError>
     where
         F: Fn(PageNo) -> bool,
     {
@@ -110,9 +113,7 @@ impl<'f, S: SqliteFile> SqliteDatabase<S> {
             }
         }
         if page_no == 0 {
-            return Err(SqliteError::Corrupt(
-                "page number cannot be zero".into(),
-            ));
+            return Err(SqliteError::Corrupt("page number cannot be zero".into()));
         } else if page_no > self.header.database_size_in_pages {
             return Err(SqliteError::Corrupt(
                 "page number is outside the database".into(),
