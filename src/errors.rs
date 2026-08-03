09812bc85c8e41bd73bbd88c760ae659b8493681
Hamza <official.hamzaderaoui@gmail.com>
2026-08-03 17:36:10 +0100

    Rename SqliteDatabaseError enum to SqliteError in errors.rs

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,6 @@
 use thiserror::Error;
 #[derive(Debug, Error)]
-pub enum SqliteDatabaseError {
+pub enum SqliteError {
     #[error("Failed to open database file: {0}")]
     DatabaseOpenFailure(#[from] std::io::Error),
 
