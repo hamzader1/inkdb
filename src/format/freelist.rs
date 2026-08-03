53af965ba5cebb782c21bc36b040c09241258b9c
Hamza <official.hamzaderaoui@gmail.com>
2026-08-03 19:18:38 +0100

    Refactor freelist and overflow pages to use Pager for page retrieval

--- src/format/freelist.rs
+++ src/format/freelist.rs
@@ -1,4 +1,5 @@
 use crate::bytes::read_u32_be;
+use crate::pager::page::Pager;
 use crate::util::sqlite_assert_one;
 use crate::vfs::file::SqliteFile;
 use crate::{DbError, SqliteDatabase};
@@ -35,18 +36,20 @@ impl<S: SqliteFile> SqliteDatabase<S> {
         }
         let mut trunk_pages = Vec::<u32>::new();
         let mut leaf_pages = Vec::<u32>::new();
-        let mut buf = vec![0u8; self.header.database_page_size as _];
+        // let mut buf = vec![0u8; self.header.database_page_size as _];
+        let max_pages = self.header.database_size_in_pages;
 
         while current_page > 0 {
-            self.read_raw_page_into(current_page, &mut buf)?;
-            let mut cursor = Cursor::new(&mut buf);
+            // self.read_raw_page_into(current_page, &mut buf)?;
+            let buf = self.pager.get(current_page)?.bytes();
+            let mut cursor = Cursor::new(&buf);
             trunk_pages.push(current_page);
             let next_freelist_trunk = read_u32_be(&mut cursor)?;
-            self.validate_page(current_page, Some(|page| page == 1))?;
+            Pager::<S>::validate_page(current_page, max_pages as _, Some(|p| p == 1))?;
             let leaf_pages_cnt = read_u32_be(&mut cursor)?;
             for _ in 0..leaf_pages_cnt {
                 let leaf_page = read_u32_be(&mut cursor)?;
-                self.validate_page(leaf_page, Some(|page| page == 1))?;
+                Pager::<S>::validate_page(leaf_page, max_pages as _, Some(|p| p == 1))?;
                 leaf_pages.push(leaf_page);
             }
             current_page = next_freelist_trunk;
