a66c440d3bbe7d54374d37266437844efc22a773
Hamza <official.hamzaderaoui@gmail.com>
2026-08-06 14:50:04 +0100

    Rename PageCursor to SqliteCursor and refactor cursor methods and reading logic

--- src/btree/page_cursor.rs
+++ src/btree/page_cursor.rs
@@ -3,11 +3,11 @@ use crate::decode_varint;
 use crate::to_int;
 use crate::SqliteError;
 
-pub struct PageCursor<'a> {
+pub struct SqliteCursor<'a> {
     bytes: &'a [u8],
     offset: u64,
 }
-impl<'a> PageCursor<'a> {
+impl<'a> SqliteCursor<'a> {
     pub fn new<S: AsRef<[u8]> + ?Sized>(bytes: &'a S) -> Self {
         Self {
             bytes: bytes.as_ref(),
@@ -15,19 +15,31 @@ impl<'a> PageCursor<'a> {
         }
     }
 
-    pub fn new_offset<S: AsRef<[u8]> + ?Sized>(bytes: &'a S, offset: u64) -> Self {
+    pub fn with_offset<S: AsRef<[u8]> + ?Sized>(bytes: &'a S, offset: u64) -> Self {
+        assert!(
+            offset as usize <= bytes.as_ref().len(),
+            "offset is bigger than the bytes length"
+        );
         Self {
             bytes: bytes.as_ref(),
             offset,
         }
     }
     pub fn set_offset(&mut self, offset: u64) {
-        self.offset = offset
+        self.offset = offset;
     }
-    fn current_pos(&self) -> u64 {
+
+    pub fn move_forward_by(&mut self, steps: u64) {
+        self.offset += steps;
+    }
+
+    pub fn move_backward_by(&mut self, steps: u64) {
+        self.offset -= steps;
+    }
+    pub fn stream_pos(&self) -> u64 {
         self.offset
     }
-    fn reset(&mut self) {
+    pub fn reset(&mut self) {
         self.offset = 0;
     }
 
@@ -60,12 +72,13 @@ impl<'a> PageCursor<'a> {
         self.read_next_exact(&mut buf);
         to_int!(u8, buf)
     }
-    pub fn read_next_arrary<const N: usize>(&mut self) -> [u8; N] {
+    pub fn read_next_array<const N: usize>(&mut self) -> [u8; N] {
         let mut buf = [0u8; N];
         self.read_next_exact(&mut buf);
         buf
     }
 
+    // read without moving the cursor
     pub fn read_at<B: AsMut<[u8]> + ?Sized>(&mut self, buf: &mut B, offset: u64) {
         let buf = buf.as_mut();
         assert!(
@@ -75,8 +88,6 @@ impl<'a> PageCursor<'a> {
         let offset = offset as usize;
         let slice = &self.bytes[offset..offset + buf.len()];
         buf.copy_from_slice(slice);
-        // TODO 2: MAY THIS CAUSE AN OVERFLOW?
-        self.offset += buf.len() as u64;
     }
 
     pub fn read_u32_at(&mut self, offset: u64) -> u32 {
@@ -85,26 +96,28 @@ impl<'a> PageCursor<'a> {
         to_int!(u32, buf)
     }
 
-    pub fn read_u16(&mut self, offset: u64) -> u16 {
+    pub fn read_u16_at(&mut self, offset: u64) -> u16 {
         let mut buf = [0u8; 2];
         self.read_at(&mut buf, offset);
         to_int!(u16, buf)
     }
 
-    pub fn read_u8(&mut self, offset: u64) -> u8 {
+    pub fn read_u8_at(&mut self, offset: u64) -> u8 {
         let mut buf = [0u8; 1];
         self.read_at(&mut buf, offset);
         to_int!(u8, buf)
     }
-    pub fn read_arrary<const N: usize>(&mut self) -> [u8; N] {
+    pub fn read_array_at<const N: usize>(&mut self) -> [u8; N] {
         let mut buf = [0u8; N];
         self.read_next_exact(&mut buf);
         buf
     }
-    pub fn read_varint_next(&self, usable_size: usize) -> (u64, usize) {
+    pub fn read_next_varint(&mut self, usable_size: usize) -> (u64, usize) {
         let remaining_bytes = self.remaining_varint_bytes(self.offset, usable_size);
         let bytes = &(&[0u8; 9])[0..remaining_bytes];
-        decode_varint(bytes).expect("Failed to read decode varint")
+        let (int, consumed) = decode_varint(bytes).expect("Failed to read decode varint");
+        self.offset += consumed as u64;
+        (int, consumed)
     }
 
     pub fn read_varint_at(&self, offset: u64, usable_size: usize) -> (u64, usize) {
@@ -120,5 +133,4 @@ impl<'a> PageCursor<'a> {
 
         remaining.min(9)
     }
-
 }
