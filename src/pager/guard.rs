10df7abef067ef631b36aff0a72fad591dc1b90d
Hamza <official.hamzaderaoui@gmail.com>
2026-08-07 19:56:09 +0100

    Merge PageGuard and PageGuardMut into a single PageGuard type using BorrowState

--- src/pager/guard.rs
+++ src/pager/guard.rs
@@ -5,26 +5,31 @@ use super::frame::Frame;
 use std::{marker::PhantomData, ptr::NonNull};
 
 #[derive(Debug)]
+pub enum BorrowState {
+    Ref,
+    RefMut,
+}
+
+#[derive(Debug)]
 pub struct PageGuard {
     buffer_pool: NonNull<BufferPool>,
     frame_id: FrameId,
     bytes: NonNull<[u8]>,
+    state: BorrowState,
     _marker: PhantomData<BufferPool>,
 }
-#[derive(Debug)]
-pub struct PageGuardMut {
-    buffer_pool: NonNull<BufferPool>,
-    frame_id: FrameId,
-    bytes: NonNull<[u8]>,
-    _marker: PhantomData<BufferPool>,
-}
-
 impl PageGuard {
-    pub fn new(buffer_pool: NonNull<BufferPool>, frame_id: FrameId, bytes: NonNull<[u8]>) -> Self {
+    pub fn new(
+        buffer_pool: NonNull<BufferPool>,
+        frame_id: FrameId,
+        bytes: NonNull<[u8]>,
+        state: BorrowState,
+    ) -> Self {
         Self {
             buffer_pool,
             frame_id,
             bytes,
+            state,
             _marker: PhantomData,
         }
     }
@@ -36,32 +41,7 @@ impl PageGuard {
     }
 }
 
-impl PageGuardMut {
-    pub fn new(buffer_pool: NonNull<BufferPool>, frame_id: FrameId, bytes: NonNull<[u8]>) -> Self {
-        Self {
-            buffer_pool,
-            frame_id,
-            bytes,
-            _marker: PhantomData,
-        }
-    }
-    pub fn bytes(&mut self) -> &mut [u8] {
-        unsafe { self.bytes.as_mut() }
-    }
-    pub fn frame_id(&self) -> FrameId {
-        self.frame_id
-    }
-}
-
 impl Drop for PageGuard {
-    fn drop(&mut self) {
-        unsafe {
-            self.buffer_pool.as_mut().free_page(self.frame_id);
-        }
-    }
-}
-
-impl Drop for PageGuardMut {
     fn drop(&mut self) {
         unsafe {
             self.buffer_pool.as_mut().free_page(self.frame_id);
