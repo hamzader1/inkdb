8d916bc0b21a458b8b0319bb3d4fefd4f695b02b
Hamza <official.hamzaderaoui@gmail.com>
2026-08-10 21:52:39 +0100

    Make parser methods public and implement token consumption and expectation helpers

--- src/sql/parser.rs
+++ src/sql/parser.rs
@@ -11,48 +11,88 @@ pub struct Parser {
 }
 
 impl Parser {
-    fn parse(tokens: Vec<Token>) -> Ast {
+    pub fn parse(tokens: Vec<Token>) -> Ast {
         let mut parser = Parser { tokens, pos: 0 };
         Ast::Null
     }
-    fn current(&self) -> Option<&TokenKind> {
+    pub fn at(&self, t_kind: &TokenKind) -> bool {
+        if let Some(t) = self.tokens.get(self.pos)
+            && &t.kind == t_kind
+        {
+            return true;
+        }
+        false
+    }
+
+    pub fn eat(&mut self, t_kind: &TokenKind) -> bool {
+        if self.at(t_kind) {
+            self.pos += 1;
+            return true;
+        }
+        false
+    }
+
+    pub fn peek(&self) -> Option<&TokenKind> {
         if let Some(t) = self.tokens.get(self.pos) {
             return Some(&t.kind);
         }
         None
     }
 
-    fn peek_ahead_by(&self, n: usize) -> Option<&TokenKind> {
-        if let Some(t) = self.tokens.get(self.pos + n) {
-            return Some(&t.kind);
-        }
-        None
+    pub fn next(&mut self) -> Option<Token> {
+        let t = self.tokens.get(self.pos).cloned();
+        self.pos += 1;
+        t
     }
 
-    fn peek(&self) -> Option<&TokenKind> {
-        if let Some(t) = self.tokens.get(self.pos) {
-            return Some(&t.kind);
+    pub fn expect(&self, t_kind: &TokenKind) {
+        if !self.at(t_kind) {
+            panic!("Token Mismatch") // temporary panic for now
         }
-        None
     }
-    fn advance(&mut self) -> Option<&TokenKind> {
-        if let Some(t) = self.tokens.get(self.pos) {
-            return Some(&t.kind);
+    pub fn expect_ident(&mut self) -> String {
+        match self.next() {
+            Some(t) => match t.kind {
+                String(x) => x,
+                _ => panic!(),
+            },
+            None => panic!(),
         }
-        None
-    }
-    fn span(&self) -> Option<&Span> {
-        if let Some(t) = self.tokens.get(self.pos) {
-            return Some(&t.span);
-        }
-        None
     }
 
-    fn parse_statment(&mut self) -> Ast {
-        match self.peek() {
-            Some(Create) => {}
-            _ => unreachable!(),
-        }
-        Ast::Null
-    }
+    // pub fn peek_ahead_by(&self, n: usize) -> Option<&TokenKind> {
+    //     if let Some(t) = self.tokens.get(self.pos + n) {
+    //         return Some(&t.kind);
+    //     }
+    //     None
+    // }
+
+    // pub fn advance(&mut self) -> Option<&TokenKind> {
+    //     if let Some(t) = self.tokens.get(self.pos) {
+    //         return Some(&t.kind);
+    //     }
+    //     None
+    // }
+
+    // pub fn verify_and_advance(&mut self, t_kind: TokenKind) -> Option<&TokenKind> {
+    //     if let Some(t) = self.tokens.get(self.pos) && t_kind == t.kind {
+    //         self.pos+=1;
+    //         return Some(&t.kind)
+    //     }
+    //     None
+    // }
+    // pub fn span(&self) -> Option<&Span> {
+    //     if let Some(t) = self.tokens.get(self.pos) {
+    //         return Some(&t.span);
+    //     }
+    //     None
+    // }
+
+    // pub fn parse_statment(&mut self) -> Ast {
+    //     // match self.advance() {
+    //         Some(Create) => {}
+    //         _ => unreachable!(),
+    //     }
+    //     Ast::Null
+    // }
 }
