e8cd2e25090efab2046db958e2b8ba43258d3f23
Hamza <official.hamzaderaoui@gmail.com>
2026-08-11 18:34:13 +0100

    implement index interior and leaf page traversal

--- src/storage/btree.rs
+++ src/storage/btree.rs
@@ -41,6 +41,10 @@ impl Path {
         }
     }
 }
+enum SearchResult {
+    Found { row_id: i64, cell_index: CellIdx },
+    Descend { child: u32, cell_index: CellIdx },
+}
 #[derive(Debug)]
 pub struct BTreeCursor {
     root: PageNo,
@@ -66,10 +70,10 @@ impl BTreeCursor {
         self.clear_path();
         let mut page_no = self.root;
         loop {
-            let guard = pager.get(page_no)?; // reponsable for bytes
-            let page = Self::page_as_ref(page_no, &guard, pager)?; // hold page
+            let guard = pager.get(page_no)?;
+            let page = Self::page_as_ref(page_no, &guard, pager)?;
             if page.is_leaf() {
-                let (found, cell_idx) = self.choose_target(&page, &target)?; // value created here and dropped at the end
+                let (found, cell_idx) = self.choose_target(&page, pager, &target)?;
                 self.stack.push(Path::new(page_no, cell_idx, guard));
                 if found {
                     return Ok(SeekResult::Exact);
@@ -77,9 +81,17 @@ impl BTreeCursor {
                 return Ok(SeekResult::NotFound);
             }
             self.state = CursorState::At;
-            let (child, cell_idx) = self.choose_child(&page, &target)?;
-            self.stack.push(Path::new(page_no, cell_idx, guard));
-            page_no = child;
+            let search_result = self.choose_child(&page, pager, &target)?;
+            match search_result {
+                SearchResult::Found { row_id, cell_index } => {
+                    self.stack.push(Path::new(page_no, cell_index, guard));
+                    return Ok(SeekResult::Exact);
+                }
+                SearchResult::Descend { child, cell_index } => {
+                    self.stack.push(Path::new(page_no, cell_index, guard));
+                    page_no = child;
+                }
+            }
         }
     }
 
@@ -239,11 +251,15 @@ impl BTreeCursor {
     fn clear_path(&mut self) {
         self.stack.clear();
     }
-    fn choose_child<'a>(
+    fn choose_child<'a, F>(
         &self,
         page: &BTreePageRef<'a>,
+        pager: &mut Pager<F>,
         target: &Value,
-    ) -> Result<(PageNo, CellIdx), SqliteError> {
+    ) -> Result<SearchResult, SqliteError>
+    where
+        F: SqliteFile,
+    {
         debug_assert!(
             page.is_interior(),
             "Navigation path of this works only with interior pages"
@@ -257,23 +273,61 @@ impl BTreeCursor {
                 let cell = page.cell(m)?;
                 let row_id = &cell.row_id().into_sqlite_value();
                 if row_id >= target {
-                    return Ok((cell.left_child(), m));
-                } else if row_id > target {
                     r = m;
                 } else {
                     l = m + 1
                 }
             }
-            return Ok((page.right_most_ptr().unwrap(), cell_count));
+            if l < cell_count {
+                let res = SearchResult::Descend {
+                    child: page.cell(l)?.left_child(),
+                    cell_index: l,
+                };
+                return Ok(res);
+            }
+        } else {
+            let mut l = 0;
+            let mut r = cell_count;
+            while l < r {
+                let m = l + (r - l) / 2;
+                let cell = page.cell(m)?;
+                let mut payload = page.record_of(&cell, pager)?;
+                let row_id = payload.pop().unwrap().get_int()?;
+                let tuple = Value::Tuple(payload);
+                if &tuple == target {
+                    return Ok(SearchResult::Found {
+                        row_id,
+                        cell_index: m,
+                    });
+                }
+                if &tuple > target {
+                    r = m;
+                } else {
+                    l = m + 1;
+                }
+            }
+            if l < cell_count {
+                return Ok(SearchResult::Descend {
+                    child: page.cell(l)?.left_child(),
+                    cell_index: l,
+                });
+            }
         }
-        todo!("INDEX INTERIOR NOT IMPLEMENTED YET")
+        Ok(SearchResult::Descend {
+            child: page.right_most_ptr().unwrap(),
+            cell_index: cell_count,
+        })
     }
 
-    fn choose_target<'a>(
+    fn choose_target<'a, F>(
         &'a self,
         page: &BTreePageRef<'a>,
+        pager: &mut Pager<F>,
         target: &Value<'_>,
-    ) -> Result<(bool, CellIdx), SqliteError> {
+    ) -> Result<(bool, CellIdx), SqliteError>
+    where
+        F: SqliteFile,
+    {
         assert!(page.is_leaf(), "This navigation path works only for leaves");
         let cell_cnt = page.no_of_cells();
         if page.page_type() == BTreePageType::LeafTable {
@@ -291,9 +345,26 @@ impl BTreeCursor {
                     l = m + 1;
                 }
             }
-            return Ok((false, l));
+            Ok((false, l))
+        } else {
+            let mut l = 0;
+            let mut r = cell_cnt;
+            while l < r {
+                let m: u16 = l + ((r - l) / 2);
+                let cell = page.cell(m)?;
+                let mut payload = page.record_of(&cell, pager)?;
+                let row_id = payload.pop().unwrap().get_int()?;
+                let tuple = Value::Tuple(payload);
+                if &tuple == target {
+                    return Ok((true, m));
+                } else if &tuple > target {
+                    r = m;
+                } else {
+                    l = m + 1;
+                }
+            }
+            Ok((false, l))
         }
-        todo!("INDEX LEAF NOT IMPLEMENTED YET")
     }
 
     pub fn current_page_as_ref<'a, F: SqliteFile>(
