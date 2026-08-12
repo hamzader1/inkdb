b04f82f00c6c2e9afd0cabdc013398ddd5b8d17c
Hamza <official.hamzaderaoui@gmail.com>
2026-08-12 12:08:54 +0100

    Add AST definitions for SQL expressions and operators

--- src/sql/ast.rs
+++ src/sql/ast.rs
@@ -20,7 +20,50 @@ pub struct Column {
     pub affinity: Affinity,
     pub constraints: Option<Vec<Constraint>>,
 }
+#[derive(Debug, Clone)]
+pub enum Expr {
+    Empty,
+    Number(i32),
+    StringLitteral(String),
+    Bool(bool),
+    Identifier(String),
+    Add(Box<Expr>, Box<Expr>),
+    Substract(Box<Expr>, Box<Expr>),
+    Devide(Box<Expr>, Box<Expr>),
+    Multiply(Box<Expr>, Box<Expr>),
+    // Statment(Stmt),
+    BinaryOp {
+        left: Box<Expr>,
+        op: BinaryOperator,
+        right: Box<Expr>,
+    },
 
+    And {
+        left: Box<Expr>,
+        right: Box<Expr>,
+    },
+
+    Or {
+        left: Box<Expr>,
+        right: Box<Expr>,
+    },
+}
+#[derive(Debug, Clone)]
+pub enum BinaryOperator {
+    Eq,
+    NotEq,
+    Ge,
+    Le,
+    Gt,
+    Lt,
+    Contains,
+}
+
+pub struct SelectStmt {
+    table_name: String,
+    columns: Vec<Column>,
+    where_clause: Option<Expr>,
+}
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Affinity {
     Text,
--- src/sql/mod.rs
+++ src/sql/mod.rs
@@ -1,5 +1,6 @@
 pub mod ast;
 pub mod lexer;
 pub mod parse_ddl;
+pub mod parse_expr;
 pub mod parser;
 mod tokens;
