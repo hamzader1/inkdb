cade675ab8bb2fa796da9f9943d32536642abe5b
Hamza <official.hamzaderaoui@gmail.com>
2026-08-13 19:21:22 +0100

    Remove Executor trait implementation from TableScan and derive Debug

--- src/backend/executor/scan.rs
+++ src/backend/executor/scan.rs
@@ -2,9 +2,10 @@ use crate::errors::SqliteError;
 use crate::record::Value;
 use crate::storage::btree::BTreeCursor;
 
-use super::{Executor, Row};
+use super::Row;
 use crate::pager::pager::Pager;
 
+#[derive(Debug)]
 pub struct TableScan {
     root_page: u32,
     cursor: BTreeCursor,
@@ -13,12 +14,11 @@ impl TableScan {
     pub fn new(root_page: u32, pager: &mut Pager) -> Self {
         let mut cursor = BTreeCursor::new(root_page);
         cursor.first(pager);
-        dbg!(&cursor);
         Self { root_page, cursor }
     }
 }
-impl Executor for TableScan {
-    fn next(&mut self, pager: &mut Pager) -> Result<Option<Row>, SqliteError> {
+impl TableScan {
+    pub fn next(&mut self, pager: &mut Pager) -> Result<Option<Row>, SqliteError> {
         if let Some(row) = self.cursor.current_record(pager)? {
             let v: Row = row.iter().map(|v| v.into_owned()).collect();
             self.cursor.next(pager)?;
