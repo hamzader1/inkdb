76486d30dd768fc4db4978a34356cc12333c7ad9
Hamza <official.hamzaderaoui@gmail.com>
2026-08-13 12:28:02 +0100

    Add expression evaluator for SQL execution engine

---
+++ src/backend/executor/eval.rs
@@ -0,0 +1,23 @@
+use std::borrow::Cow;
+
+use crate::record::Value;
+use crate::sql::ast::Expr;
+use crate::sql::parser::Arena;
+
+pub struct Eval;
+impl Eval {
+    pub fn eval(arena: &Arena, idx: usize, row: &[Value<'static>]) -> Value<'static> {
+        match arena.nodes[idx] {
+            Expr::Number(n) => Value::Integer(n),
+            Expr::Float(f) => Value::Float(f),
+            Expr::StringLitteral(ref str) => Value::Text(Cow::Owned(str.to_string())),
+            Expr::Bool(b) => Value::Integer(b as u8 as i64),
+            Expr::ColumnRef(col_idx) => row[col_idx].into_owned(),
+            Expr::Add(l, r) => Self::eval(arena, l, row) + Self::eval(arena, r, row),
+            Expr::Substract(l, r) => Self::eval(arena, l, row),
+            Expr::Multiply(l, r) => Self::eval(arena, l, row),
+            Expr::Devide(l, r) => Self::eval(arena, l, row),
+            _ => todo!(),
+        }
+    }
+}
