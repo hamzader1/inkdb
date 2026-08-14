91c9abba6a33ff07ee68c13bce920e43917b6f3e
Hamza <official.hamzaderaoui@gmail.com>
2026-08-14 01:52:41 +0100

    Add filter plan to execution planner for where clause handling

--- src/backend/planner/plan.rs
+++ src/backend/planner/plan.rs
@@ -1,6 +1,7 @@
 use super::super::executor::{project::Project, scan::TableScan};
 use crate::backend::analyze::ResolvedQuery;
-use crate::backend::executor::{Row};
+use crate::backend::executor::Row;
+use crate::backend::executor::filter::Filter;
 use crate::errors::SqliteError;
 use crate::pager::pager::Pager;
 use crate::sql::parser::Arena;
@@ -9,21 +10,21 @@ use crate::vfs::file::SqliteFile;
 #[derive(Debug)]
 pub enum Plan {
     TableScan(TableScan),
+    Filter(Filter),
     Project(Project),
 }
 impl Plan {
     pub fn create_plan(resolved_query: ResolvedQuery, pager: &mut Pager) -> Self {
-        let table = Self::TableScan(TableScan::new(resolved_query.root_page, pager));
-
-        /*
-         *
-         *  TODO: missing where clause
-         *
-         *
-         */
-
+        let mut child = Self::TableScan(TableScan::new(resolved_query.root_page, pager));
+        if let Some(predict) = resolved_query.where_clause {
+            child = Self::Filter(Filter::new(
+                Box::new(child),
+                resolved_query.arena.clone(),
+                predict,
+            ));
+        }
         Self::Project(Project::new(
-            Box::new(table),
+            Box::new(child),
             resolved_query.arena.clone(),
             resolved_query.columns.clone(),
         ))
@@ -34,6 +35,7 @@ impl Plan {
     pub fn next(&mut self, pager: &mut Pager) -> Result<Option<Row>, SqliteError> {
         match self {
             Self::TableScan(t) => t.next(pager),
+            Self::Filter(f) => f.next(pager),
             Self::Project(p) => p.next(pager),
         }
     }
