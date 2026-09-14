c04d50ff9a56633e160d1872016471cd8fd53430
Hamza <official.hamzaderaoui@gmail.com>
2026-09-14 23:52:16 +0100

    Remove unused counter and debug logging from index exact match executor

--- src/backend/executor/index.rs
+++ src/backend/executor/index.rs
@@ -138,7 +138,6 @@ pub struct IndexExactMatch<F: SqliteFile> {
     relation_root_page: u32,
     target: Value<'static>,
     cursor: BTreeCursor<F>,
-    cnt: usize,
     is_done: bool,
 }
 
@@ -161,13 +160,10 @@ impl<F: SqliteFile> IndexExactMatch<F> {
             relation_root_page,
             target,
             cursor,
-            cnt: 0,
             is_done: seek_res == SeekResult::NotFound,
         })
     }
     pub fn next(&mut self, pager: &mut Pager<F>, arena: &ExprArena) -> SqliteResult<Option<Row>> {
-        self.cnt += 1;
-
         match self.cursor.restore_position(pager)? {
             RestorePosition::Exact => {
                 self.cursor.next(pager)?;
@@ -182,10 +178,6 @@ impl<F: SqliteFile> IndexExactMatch<F> {
 
         let current_index_record = self.cursor.current_record(pager)?;
         let Some(mut index_record) = current_index_record else {
-            eprintln!(
-                "IndexExactMatch DONE current_record None stack {:?}",
-                self.cursor.stack
-            );
             self.is_done = true;
             return Ok(None);
         };
@@ -196,11 +188,6 @@ impl<F: SqliteFile> IndexExactMatch<F> {
             .into_owned();
 
         if index_record[0] != self.target {
-            eprintln!(
-                "IndexExactMatch DONE mismatch: got {:?} target {:?}",
-                index_record[0], self.target
-            );
-            eprintln!("stack {:?}", self.cursor.stack);
             self.is_done = true;
             return Ok(None);
         }
