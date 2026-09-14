82a0645d61fa63877419dbfd7a20b5acb3fc2852
Hamza <official.hamzaderaoui@gmail.com>
2026-09-14 23:52:06 +0100

    Execute row deletions immediately during iteration instead of buffering all keys

--- src/backend/executor/delete.rs
+++ src/backend/executor/delete.rs
@@ -3,7 +3,7 @@ use crate::backend::planner::plan::Plan;
 use crate::pager::pager::{PageNo, Pager};
 use crate::record::SqlType;
 use crate::sql::parser::ExprArena;
-use crate::storage::btree::BTree;
+use crate::storage::btree::{BTree, BTreeCursor};
 use crate::vfs::file::SqliteFile;
 
 use super::Row;
@@ -27,13 +27,9 @@ impl<F: SqliteFile> Delete<F> {
         pager: &mut Pager<F>,
         arena: Option<&ExprArena>,
     ) -> SqliteResult<Option<Row>> {
-        let mut ids = Vec::new();
         while let Some(row) = self.child.next(pager, arena)? {
-            ids.push(row.key);
-        }
-        let mut btree = BTree::new(self.root_page, pager);
-        for key in ids {
-            btree.delete(key.into_sqlite_value())?;
+            let mut btree = BTree::new(self.root_page, pager);
+            btree.delete(row.key.into_sqlite_value())?;
         }
         Ok(None)
     }
